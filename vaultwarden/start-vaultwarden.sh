026311e2af73af4bbefa9126dff53cc4e52791f5
zhanglikun <zhanglikun@nutstore.net>
2021-09-09 10:57:55 +0800

    fix vaultwarden build

--- vaultwarden/start-vaultwarden.sh
+++ vaultwarden/start-vaultwarden.sh
@@ -1,11 +1,9 @@
-VAULTWARDEN_DATA=$(pwd)/bitwarden-data/
-
 docker run -d \
--v $VAULTWARDEN_DATA:/data/ \
--p 127.0.0.1:10001:80 \
--p 127.0.0.1:10002:3012 \
---env-file=$VAULTWARDEN_DATA/vaultwarden.env \
---name=vaultwarden \
---restart=always \
-vaultwarden/server
-
+    -v $(pwd)/vaultwarden_data/:/data/ \
+    -p 127.0.0.1:10001:80 \
+    -p 127.0.0.1:10002:3012 \
+    --env-file=$(pwd)/vaultwarden.env \
+    --name=vaultwarden \
+    --restart=always \
+    vaultwarden/server
+    
