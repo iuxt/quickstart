1faf533bb3eb6b1de39d212eafe749207bbcfbc9
zhanglikun <iuxt@qq.com>
2021-11-02 11:13:24 +0800

    add default databases create on first use

--- mysql/run_mysql_server.sh
+++ mysql/run_mysql_server.sh
@@ -1,6 +1,7 @@
 source ./.env
 docker run --name mysql \
        -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+       -e MYSQL_DATABASE=${MYSQL_DATABASE} \
        -p ${MYSQL_PORT}:3306 \
        -v ${MYSQL_DATA}:/var/lib/mysql \
        -d mysql:${MYSQL_VERSION}
