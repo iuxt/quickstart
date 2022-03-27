b28fb34226a8ea0ac6509a94591722fa185ac5b3
zhanglikun <iuxt@qq.com>
2022-03-27 12:11:10 +0000

    add create docker network

--- mysql/run_mysql_server.sh
+++ mysql/run_mysql_server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 source .env
+
+if [ $(docker network ls | grep -c iuxt) -eq 0 ]; then
+  docker network create iuxt
+else
+  echo "docker network iuxt exists skip"
+fi
+
 docker run --name mysql \
        -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
        -e MYSQL_DATABASE=${MYSQL_DATABASE} \
        -p ${MYSQL_PORT}:3306 \
        -v ${MYSQL_DATA}:/var/lib/mysql \
+       --network iuxt \
        -d mysql:${MYSQL_VERSION}
