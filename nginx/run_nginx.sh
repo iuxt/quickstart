b6a8134706a69542dd1eded8613dd46518d1bc94
zhanglikun <iuxt@qq.com>
2022-03-27 14:17:58 +0000

    add ssl support for nginx

--- nginx/run_nginx.sh
+++ nginx/run_nginx.sh
@@ -7,6 +7,7 @@ docker run --name nginx \
   -v $(pwd)/www:/usr/share/nginx/html:ro \
   -v $(pwd)/conf/nginx.conf:/etc/nginx/nginx.conf \
   -v $(pwd)/conf/conf.d:/etc/nginx/conf.d \
+  -v $(pwd)/conf/ssl:/etc/nginx/ssl \
   -p ${HTTP_PORT}:80 \
   -p ${HTTPS_PORT}:443 \
   --network iuxt \
