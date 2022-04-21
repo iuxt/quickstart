60de4a512d1894b66306b5a7d30719d5fc41bf33
Ubuntu <ubuntu@zhangfei.zahui.fan>
2022-04-21 04:49:51 +0000

    add env and script

--- docker-ipsec-vpn-server/run_vpn_server.sh
+++ docker-ipsec-vpn-server/run_vpn_server.sh
@@ -5,6 +5,7 @@ docker run \
   --name ipsec-vpn-server \
   --restart=always \
   -v "$(pwd)"/ikev2-vpn-data:/etc/ipsec.d \
+  --env-file=.env \
   -p 500:500/udp \
   -p 4500:4500/udp \
   -d --privileged \
