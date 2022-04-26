0aad752064a4762da20223b3d4679d3e6405020d
zhanglikun <iuxt@qq.com>
2022-04-26 02:04:04 +0000

    remove https in container

--- gitlab-arm64/run_gitlab.sh
+++ gitlab-arm64/run_gitlab.sh
@@ -6,12 +6,11 @@ docker run \
   --memory 4096M \
   --network iuxt \
   --publish 2222:22 \
-  --publish 2080:80 \
-  --publish 2443:443 \
   --hostname gitlab.babudiu.com \
   --env GITLAB_OMNIBUS_CONFIG=" \
-    nginx['redirect_http_to_https'] = false; "\
-  --volume /srv/gitlab-ce/conf:/etc/gitlab:z \
-  --volume /srv/gitlab-ce/logs:/var/log/gitlab:z \
-  --volume /srv/gitlab-ce/data:/var/opt/gitlab:z \
+    nginx['redirect_http_to_https'] = false; 
+    nginx['listen_https'] = nil" \
+  --volume "$PWD"/conf:/etc/gitlab:z \
+  --volume "$PWD"/logs:/var/log/gitlab:z \
+  --volume "$PWD"/data:/var/opt/gitlab:z \
   yrzr/gitlab-ce-arm64v8:latest
