945962864be47d4ba64fc3c89f0901a6d14d92f5
zhanglikun <iuxt@qq.com>
2022-05-01 03:00:26 +0000

    add restart policy change menory limit

--- gitlab-arm64/run_gitlab.sh
+++ gitlab-arm64/run_gitlab.sh
@@ -3,15 +3,16 @@ docker run \
   --restart always \
   --name gitlab-ce \
   --privileged \
-  --memory 4096M \
+  --memory 8192M \
   --network iuxt \
   --publish 2222:22 \
   --hostname gitlab.babudiu.com \
   --env GITLAB_OMNIBUS_CONFIG=" \
     gitlab_rails['gitlab_shell_ssh_port'] = 2222
     nginx['redirect_http_to_https'] = false 
-    nginx['listen_https'] = nil" \
+    " \
   --volume "$PWD"/conf:/etc/gitlab:z \
   --volume "$PWD"/logs:/var/log/gitlab:z \
   --volume "$PWD"/data:/var/opt/gitlab:z \
+  --restart always \
   yrzr/gitlab-ce-arm64v8:latest
