d6631f2c85f38fa55994c85b70287195a2d70f5e
zhanglikun <iuxt@qq.com>
2022-05-03 23:13:58 +0800

    add aria2

---
+++ aria2/readme.md
@@ -0,0 +1,7 @@
+# aria2
+
+github：<https://github.com/P3TERX/Aria2-Pro-Docker>
+
+文档说明：<https://p3terx.com/archives/docker-aria2-pro.html>
+
+AriaNG 前端：<https://github.com/mayswind/AriaNg>
---
+++ aria2/run_aria2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+docker run -d \
+    --name aria2-pro \
+    --restart unless-stopped \
+    --log-opt max-size=1m \
+    -e PUID=$UID \
+    -e PGID=$GID \
+    -e UMASK_SET=022 \
+    -e RPC_SECRET=com.012 \
+    -e RPC_PORT=6800 \
+    -p 6800:6800 \
+    -e LISTEN_PORT=6888 \
+    -p 6888:6888 \
+    -p 6888:6888/udp \
+    -v $PWD/aria2-config:/config \
+    -v $PWD/aria2-downloads:/downloads \
+    p3terx/aria2-pro
