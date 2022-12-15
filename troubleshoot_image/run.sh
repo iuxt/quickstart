25a3c03384a034b53fea6e316d2db634fba3032a
zhanglikun <iuxt@qq.com>
2022-12-15 14:55:40 +0800

    增加kubectl使用教程

--- troubleshoot_image/run.sh
+++ troubleshoot_image/run.sh
@@ -8,3 +8,4 @@ else
 fi
 
 docker run -it --rm --network iuxt registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1 bash
+# kubectl run -i --tty manager --image=registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1 --restart=Never /bin/bash
