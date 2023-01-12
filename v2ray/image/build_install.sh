939dacffb5807340d383d8674d3952e8203a337e
zhanglikun <x@zahui.fan>
2023-01-12 22:46:44 +0800

    合并Dockerfile

---
+++ v2ray/image/build_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir /v2ray && cd /v2ray
+
+if [ $(uname -m) == "x86_64" ]; then
+    echo "x86"
+    wget https://github.com/v2fly/v2ray-core/releases/download/v5.2.0/v2ray-linux-64.zip
+    unzip v2ray-linux-64.zip && rm -f v2ray-linux-64.zip
+elif [ $(uname -m) == "aarch64" ]; then  
+    echo "arm"
+    wget https://github.com/v2fly/v2ray-core/releases/download/v5.2.0/v2ray-linux-arm64-v8a.zip
+    unzip v2ray-linux-64.zip && rm -f v2ray-linux-64.zip
+fi
--- v2ray/run_v2ray.sh
+++ v2ray/run_v2ray.sh
@@ -10,5 +10,6 @@ fi
 docker run -d --name v2ray \
   --network iuxt \
   --restart always \
+  --mount type=bind,source=$(pwd)/config.json,target=/v2ray/config.json \
   iuxt/v2ray
 
