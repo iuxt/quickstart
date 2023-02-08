ebb83e78104c02462d5592c25af79ad6c16306e8
zhanglikun <x@zahui.fan>
2023-02-08 17:41:29 +0800

    add comment

--- ddns/ipv6_addr.py
+++ ddns/ipv6_addr.py
@@ -3,6 +3,7 @@ import requests
 
 def get_ipv6_addr():
     ipv6_addr = str(os.popen('ip a | grep "inet6.*global" | awk \'{print $2}\' | grep -v "^fe80::"').read())
+    # cat /proc/net/if_inet6
     ipv6_addr = ipv6_addr.split("/")[0]
     print("ipv6_addr:", ipv6_addr)
     return ipv6_addr
