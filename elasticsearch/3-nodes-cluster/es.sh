8e2c4fb8731cbf6d75247470430326cbe87d0528
zhanglikun <x@zahui.fan>
2023-05-23 14:17:24 +0800

    手动创建目录

--- elasticsearch/3-nodes-cluster/es.sh
+++ elasticsearch/3-nodes-cluster/es.sh
@@ -3,13 +3,16 @@ sudo sysctl -w vm.max_map_count=262144
 
 docker network create --subnet=172.16.0.0/24 elasticsearch-br0
 
+mkdir data{1..3}
+mkdir logs{1..3}
+chown -R 1000:1000 data* logs*
 
 docker run -d --name elasticsearch1 \
     -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
     -v "$(pwd)"/elasticsearch1.yml:/usr/share/elasticsearch/config/elasticsearch.yml:ro \
     -v "$(pwd)"/data1:/usr/share/elasticsearch/data:rw \
     -v "$(pwd)"/logs1:/usr/share/elasticsearch/logs:rw \
-    --privileged --network elasticsearch-br0 \
+    --network elasticsearch-br0 \
     --ip 172.16.0.11 \
     -p 9201:9200 -p 9301:9300 \
     elasticsearch:7.16.2
@@ -19,7 +22,7 @@ docker run -d --name elasticsearch2 \
     -v "$(pwd)"/elasticsearch2.yml:/usr/share/elasticsearch/config/elasticsearch.yml:ro \
     -v "$(pwd)"/data2:/usr/share/elasticsearch/data:rw \
     -v "$(pwd)"/logs2:/usr/share/elasticsearch/logs:rw \
-    --privileged --network elasticsearch-br0 \
+    --network elasticsearch-br0 \
     --ip 172.16.0.12 \
     -p 9202:9200 -p 9302:9300 \
     elasticsearch:7.16.2
@@ -29,7 +32,7 @@ docker run -d --name elasticsearch3 \
     -v "$(pwd)"/elasticsearch3.yml:/usr/share/elasticsearch/config/elasticsearch.yml:ro \
     -v "$(pwd)"/data3:/usr/share/elasticsearch/data:rw \
     -v "$(pwd)"/logs3:/usr/share/elasticsearch/logs:rw \
-    --privileged --network elasticsearch-br0 \
+    --network elasticsearch-br0 \
     --ip 172.16.0.13 \
     -p 9203:9200 -p 9303:9300 \
     elasticsearch:7.16.2
