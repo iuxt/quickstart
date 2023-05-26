35da91d52e2adedde6af8eb78b8d6795b8527227
zhanglikun <x@zahui.fan>
2023-05-26 11:20:09 +0800

    增加jenkins

---
+++ jenkins/start_jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -d \
+    -v jenkins_home:/var/jenkins_home \
+    -p 8080:8080 -p 50000:50000 \
+    --name jenkins \
+    --restart=on-failure \
+    jenkins/jenkins:lts-jdk11
