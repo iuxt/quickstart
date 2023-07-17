4a14b0ca0baaddd298a57f5c409a893da28075cc
zhanglikun <x@zahui.fan>
2023-07-17 11:29:57 +0800

    增加脚本控制

---
+++ nginx/sites-available/enable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+help() {
+    cat <<-EOF
+使用方法：
+./enable -c xxx.conf
+EOF
+}
+
+[ $# -eq 0 ] && help && exit 0
+
+while getopts "ab:c:" arg           #选项后面的冒号表示该选项需要参数
+do
+    case $arg in
+        a)
+            echo "a $OPTARG"    #参数存在$OPTARG中
+            echo a
+        ;;
+        b)
+            echo "b $OPTARG"
+        ;;
+        c)
+            echo "c $OPTARG"
+            ln -sf $(pwd)/$OPTARG ../conf.d/
+        ;;
+        ?)                     #当有不认识的选项的时候arg为?
+            help
+            exit 1
+        ;;
+    esac
+done
