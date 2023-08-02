9800a7d352c3f47f0fc9aa19dab3f1b070bcda51
zhanglikun <x@zahui.fan>
2023-08-02 10:50:27 +0800

    增加gitea

--- gitea/run.sh
+++ gitea/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+../public/docker-network.sh
 
+# https://docs.gitea.com/installation/install-with-docker
+
+docker run -d \
+    --name gitea \
+    --network iuxt \
+    -e USER_UID=$UID \
+    -e USER_GID=$(id -g) \
+    --mount type=bind,source=/etc/timezone,target=/etc/timezone,readonly \
+    --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+    -v $PWD/gitea-data:/data \
+    --restart=always \
+    gitea/gitea:latest
+
+cp -f ./gitea-nginx.conf ../nginx/conf.d/
+cp -f ./gitea-nginx-stream.conf ../nginx/stream.d/
