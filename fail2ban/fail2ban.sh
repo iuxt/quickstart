be0e1913bb92263b7c0f187f4631bf724147fc5a
zhanglikun <x@zahui.fan>
2023-08-02 15:41:48 +0800

    优化fail2ban设置脚本

--- fail2ban/fail2ban.sh
+++ fail2ban/fail2ban.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-service=(
-frps
-nginx-cc
-)
+# frps
+cp -f filter.d/frps.conf /etc/fail2ban/filter.d/
+cp -f jail.d/frps.conf /etc/fail2ban/jail.d/
+container_id=$(docker inspect --format="{{.Id}}" frps)
+logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
+sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/frps.conf
 
-for i in ${service[*]};
-do
+# nginx
+cp -f filter.d/nginx-cc.conf /etc/fail2ban/filter.d/
+cp -f jail.d/nginx-cc.conf /etc/fail2ban/jail.d/
+container_id=$(docker inspect --format="{{.Id}}" nginx)
+logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
+sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/nginx-cc.conf
 
-    cp -f filter.d/${i}.conf /etc/fail2ban/filter.d/
-    cp -f jail.d/${i}.conf /etc/fail2ban/jail.d/
 
-    if [ ${i} == "nginx-cc" ]; then
-        container_id=$(docker inspect --format="{{.Id}}" nginx)
-    else
-        container_id=$(docker inspect --format="{{.Id}}" ${i})
-    fi
-
-    logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
-    sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/${i}.conf
-
-    systemctl reload fail2ban
-    fail2ban-client status ${i}
-done
+systemctl reload fail2ban
+fail2ban-client status frps
+fail2ban-client status nginx-cc
 
