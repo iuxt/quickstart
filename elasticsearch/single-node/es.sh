645b42f22dabe28d4cd6625022b2c4ba36f7b333
zhanglikun <x@zahui.fan>
2023-08-24 09:55:26 +0800

    es数据持久化，更新es和kibana版本

--- elasticsearch/single-node/es.sh
+++ elasticsearch/single-node/es.sh
@@ -4,12 +4,14 @@ docker run -d --name elasticsearch \
     -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
     -e "discovery.type=single-node" \
     -v "$(pwd)"/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml:ro \
+    -v "$(pwd)"/es-data:/usr/share/elasticsearch/data:rw \
+    -v "$(pwd)"/es-logs:/usr/share/elasticsearch/logs:rw \
     --privileged --network elasticsearch \
     -p 9200:9200 -p 9300:9300 \
-    elasticsearch:7.8.0
+    elasticsearch:7.16.2
 
 docker run -d --name kibana \
     --net elasticsearch \
     -p 5601:5601 \
     -v "$(pwd)"/kibana.yml:/usr/share/kibana/config/kibana.yml \
-    kibana:7.8.0
+    kibana:7.16.2
