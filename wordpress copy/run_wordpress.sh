1462b90c229266510a38985784cda41638d6b19b
zhanglikun <x@zahui.fan>
2023-11-01 11:27:02 +0800

    fix

--- wordpress copy/run_wordpress.sh
+++ wordpress copy/run_wordpress.sh
@@ -4,14 +4,6 @@ set -euo pipefail
 ../public/docker-network.sh
 source .env
 
-docker run -d --name wordpress-mysql \
-    --env-file=.env \
-    -v "$(pwd)/mysql_data":/var/lib/mysql \
-    -v "$(pwd)/mysql_temp":/mysql_temp \
-    --network iuxt \
-    --restart always \
-    -d mysql:"${MYSQL_VERSION}"
-
 docker run -d --name wordpress \
   -v "$PWD"/wordpress_data:/var/www/html \
   --mount type=bind,source="$PWD/php_custom.ini",target=/usr/local/etc/php/conf.d/php_custom.ini \
