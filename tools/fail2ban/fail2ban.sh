2104a3bda9d30b6909563085905dc9bf24646bfe
zhanglikun <x@zahui.fan>
2023-11-01 15:29:40 +0800

    新增nginx stream 限流配置

--- tools/fail2ban/fail2ban.sh
+++ tools/fail2ban/fail2ban.sh
@@ -7,15 +7,21 @@ container_id=$(docker inspect --format="{{.Id}}" frps)
 logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
 sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/frps.conf
 
-# nginx
-cp -f filter.d/nginx-cc.conf /etc/fail2ban/filter.d/
-cp -f jail.d/nginx-cc.conf /etc/fail2ban/jail.d/
+# nginx-http-cc
+cp -f filter.d/nginx-http-cc.conf /etc/fail2ban/filter.d/
+cp -f jail.d/nginx-http-cc.conf /etc/fail2ban/jail.d/
 container_id=$(docker inspect --format="{{.Id}}" nginx)
 logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
-sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/nginx-cc.conf
+sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/nginx-http-cc.conf
 
+# nginx-stream-cc
+cp -f filter.d/nginx-stream-cc.conf /etc/fail2ban/filter.d/
+cp -f jail.d/nginx-stream-cc.conf /etc/fail2ban/jail.d/
+container_id=$(docker inspect --format="{{.Id}}" nginx)
+logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
+sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/nginx-stream-cc.conf
 
 systemctl reload fail2ban
 fail2ban-client status frps
-fail2ban-client status nginx-cc
-
+fail2ban-client status nginx-http-cc
+fail2ban-client status nginx-stream-cc
