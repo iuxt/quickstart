5d9a4537180c74be31172603a182d08b76b1ed11
zhanglikun <x@zahui.fan>
2023-11-09 12:58:45 +0800

    增加php配置模板

--- wordpress/run_wordpress.sh
+++ wordpress/run_wordpress.sh
@@ -4,8 +4,14 @@ set -euo pipefail
 ../public/docker-network.sh
 source .env
 
+if [ ! -e ./php_custom.ini ];then
+    cp ./php_custom.ini.example ./php_custom.ini
+fi
+
+
 docker run -d --name wordpress \
   -v "$PWD"/wordpress_data:/var/www/html \
+  -v "$PWD"/custom_extensions:/usr/local/lib/php/extensions/custom_extensions \
   --mount type=bind,source="$PWD/php_custom.ini",target=/usr/local/etc/php/conf.d/php_custom.ini \
   --env-file=.env \
   --network iuxt \
