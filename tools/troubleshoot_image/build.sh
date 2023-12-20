b0d4c2618520e4666538aa1f76b09e437b0edfd0
zhanglikun <likun.zhang@ingeek.com>
2023-12-20 14:06:52 +0800

    remove troubleshoot image

--- tools/troubleshoot_image/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-docker build . -t registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1
-docker push registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1
-
--- tools/troubleshoot_image/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-../public/docker-network.sh
-
-docker run -it --rm --network iuxt registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1 bash
-# kubectl run -i --tty manager --image=registry.cn-hangzhou.aliyuncs.com/iuxt/admin_client:v1 --restart=Never /bin/bash
