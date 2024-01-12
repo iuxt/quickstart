188d1da4a222f646ab7d9e2473c4524449c20b64
Ubuntu <ubuntu@lvbu.babudiu.com>
2024-01-12 09:38:45 +0000

    规范命名

--- vaultwarden/start_vaultwarden.sh
+++ vaultwarden/start_vaultwarden.sh
@@ -2,8 +2,8 @@ docker run -d \
     -v $(pwd)/vaultwarden_data/:/data/ \
     -p 127.0.0.1:10001:80 \
     -p 127.0.0.1:10002:3012 \
-    --env-file=$(pwd)/vaultwarden.env \
+    --env-file=$(pwd)/.env \
     --name=vaultwarden \
     --restart=always \
     vaultwarden/server
-    
+
