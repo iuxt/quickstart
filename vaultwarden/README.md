7f3e7470b47992d7c69a944f6e5a647f57e35034
Ubuntu <ubuntu@lvbu.babudiu.com>
2024-01-12 09:52:58 +0000

    使用docker网络

--- vaultwarden/README.md
+++ vaultwarden/README.md
@@ -1,6 +1,6 @@
 ## 1. 修改配置文件
 
-cp vaultwarden.env.example vaultwarden.env
+cp env.example .env
 修改里面的DOMAIN
 
 
@@ -9,3 +9,6 @@ jail.d里面需要修改log路径
 ## 2. certbot创建证书
 
 别忘了创建证书，强制https访问
+
+
+端口是80和3012
--- vaultwarden/start_vaultwarden.sh
+++ vaultwarden/start_vaultwarden.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
+../public/docker-network.sh
+
 docker run -d \
     -v $(pwd)/vaultwarden_data/:/data/ \
-    -p 127.0.0.1:10001:80 \
-    -p 127.0.0.1:10002:3012 \
     --env-file=$(pwd)/.env \
     --name=vaultwarden \
     --restart=always \
