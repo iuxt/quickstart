c4e76c227f279626e98a8bc6e6b8d6568acfafdc
zhanglikun <x@zahui.fan>
2024-01-14 11:28:47 +0800

    增加vaultwarden的fail2ban配置

---
+++ vaultwarden/fail2ban/set_fail2ban.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $(id -u) != "0" ]; then
+    echo "Error: 必须使用ROOT用户来运行， 你可以在命令前面加上 sudo "
+    exit 1
+fi
+
+# vaultwarden
+cp -f filter.d/vaultwarden.conf /etc/fail2ban/filter.d/
+cp -f jail.d/vaultwarden.conf /etc/fail2ban/jail.d/
+logpath=$(pwd)/../vaultwarden_data/vaultwarden.log
+sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/vaultwarden.conf
+
+systemctl reload fail2ban
+fail2ban-client status vaultwarden
