8f6de8885d103e9f078ede193872496e8799624a
Ubuntu <ubuntu@lvbu.babudiu.com>
2024-01-14 03:02:12 +0000

    vaultwarden配置docker网络

--- vaultwarden/start_vaultwarden.sh
+++ vaultwarden/start_vaultwarden.sh
@@ -5,6 +5,7 @@
 docker run -d \
     -v $(pwd)/vaultwarden_data/:/data/ \
     --env-file=$(pwd)/.env \
+    --network iuxt \
     --name=vaultwarden \
     --restart=always \
     vaultwarden/server
