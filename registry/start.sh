043e474ad7fd78fa1a25d99d203f233320049f26
zhanglikun <x@zahui.fan>
2024-04-18 15:09:48 +0800

    对数据进行持久化

--- registry/start.sh
+++ registry/start.sh
@@ -4,7 +4,7 @@
 # docker run -d \
 #   --restart=always \
 #   --name registry \
-#   -v $(pwd)/data:/var/lib/registry \
+#   -v "$(pwd)"/data:/var/lib/registry \
 #   -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
 #   -p 5000:5000 \
 #   registry:2
@@ -15,6 +15,7 @@ docker run -d \
   --restart=always \
   --name registry \
   -v "$(pwd)"/certs:/certs \
+  -v "$(pwd)"/data:/var/lib/registry \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/server.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/server.key \
