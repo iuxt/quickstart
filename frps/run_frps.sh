7588fcedda4a60418ad77401aea308991bc1d273
zhanglikun <iuxt@qq.com>
2024-04-22 14:31:48 +0800

    upgrade frps

--- frps/run_frps.sh
+++ frps/run_frps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+docker rm -f frps
+
 docker run --name frps \
   -v "$(pwd)"/frps.toml:/etc/frp/frps.toml \
   --network host \
   --restart always \
   --log-opt max-size=1G \
   -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone \
-  -d snowdreamtech/frps:0.53.2
+  -d snowdreamtech/frps:0.57.0
