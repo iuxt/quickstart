2d23843e6386a795b446f738f76b8dd6ba71b96c
zhanglikun <x@zahui.fan>
2024-04-25 19:02:28 +0800

    更新es版本

--- elasticsearch/3-nodes-cluster/es.sh
+++ elasticsearch/3-nodes-cluster/es.sh
@@ -11,7 +11,7 @@ docker run -d --name elasticsearch1 \
     --network elasticsearch-br0 \
     --ip 172.16.0.11 \
     -p 9201:9200 -p 9301:9300 \
-    elasticsearch:7.16.2
+    elasticsearch:7.17.14
 
 docker run -d --name elasticsearch2 \
     -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
@@ -21,7 +21,7 @@ docker run -d --name elasticsearch2 \
     --network elasticsearch-br0 \
     --ip 172.16.0.12 \
     -p 9202:9200 -p 9302:9300 \
-    elasticsearch:7.16.2
+    elasticsearch:7.17.14
 
 docker run -d --name elasticsearch3 \
     -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
@@ -31,10 +31,10 @@ docker run -d --name elasticsearch3 \
     --network elasticsearch-br0 \
     --ip 172.16.0.13 \
     -p 9203:9200 -p 9303:9300 \
-    elasticsearch:7.16.2
+    elasticsearch:7.17.14
 
 docker run -d --name kibana \
     --net elasticsearch-br0 \
     -p 5601:5601 \
     -v "$(pwd)"/kibana.yml:/usr/share/kibana/config/kibana.yml \
-    kibana:7.16.2
+    kibana:7.17.14
