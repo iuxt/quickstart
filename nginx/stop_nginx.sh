9050405005f32fa6aac8785d4cd72e5e7be99e29
zhanglikun <iuxt@qq.com>
2024-06-03 15:01:58 +0800

    fix

--- nginx/stop_nginx.sh
+++ nginx/stop_nginx.sh
@@ -6,6 +6,7 @@ rm -f /etc/fail2ban/jail.d/nginx-stream-cc.conf
 rm -f /etc/fail2ban/jail.d/nginx-http-cc.conf
 
 systemctl enable fail2ban
-systemctl restart fail2ban
+systemctl reload fail2ban
 
 fail2ban-client status
+
