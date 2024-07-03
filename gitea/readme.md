1e306d5b75df820d21711db55cca8faa627a3aa9
zhanglikun <x@zahui.fan>
2024-07-03 10:48:39 +0800

    test

--- gitea/readme.md
+++ gitea/readme.md
@@ -1,6 +1,11 @@
-https://docs.gitea.com/next/administration/config-cheat-sheet
+# 说明
+
+## 官方文档
+
+<https://docs.gitea.com/next/administration/config-cheat-sheet>
 
 ## 如何使用ssh
+
 gitea-data/gitea/conf/app.ini 在server下面
 
 启用内置的ssh服务器
@@ -17,4 +22,4 @@ SSH_LISTEN_HOST = 22222
 ## 注意
 
 .env里面的内容只在第一次启动的时候有效, 如果不生效, 可以直接修改app.ini或者删除app.ini, 重新启动容器会自动生成app.ini
-app.ini 位置: quickstart/gitea/gitea-data/gitea/conf/app.ini
+app.ini 位置: gitea-data/gitea/conf/app.ini
--- gitea/run.sh
+++ gitea/run.sh
@@ -2,20 +2,20 @@
 
 ../public/docker-network.sh
 
-useradd -m iuxt
 # https://docs.gitea.com/installation/install-with-docker
 #    -m 512M --memory-swap=768M \
 
 docker run -d \
     --name gitea \
     --network iuxt \
-    -e USER_UID=$(id -u iuxt) \
-    -e USER_GID=$(id -g iuxt) \
+    -e USER_UID=1000 \
+    -e USER_GID=1000 \
     --env-file=.env \
     --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-    -v $PWD/gitea-data:/data \
+    -v "$PWD"/gitea-data:/data \
     --restart=always \
-    gitea/gitea:1.21.11
+    gitea/gitea:1.22.0
 
 cp -f ./gitea-nginx.conf ../nginx/conf.d/gitea.conf
 cp -f ./gitea-nginx-stream.conf ../nginx/stream.d/gitea.conf
+../nginx/reload_nginx.sh
