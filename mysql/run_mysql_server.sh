a70f2730332fb5ba2beea48f7ec30218558e7536
zhanglikun <x@zahui.fan>
2024-08-02 16:53:04 +0800

    增加init sql 和自定义配置文件支持

--- mysql/run_mysql_server.sh
+++ mysql/run_mysql_server.sh
@@ -10,6 +10,8 @@ docker run --name mysql \
        -e MYSQL_DATABASE="${MYSQL_DATABASE}" \
        -v "${MYSQL_DATA}":/var/lib/mysql \
        -v "$(pwd)/mysql_temp":/mysql_temp \
+       -v "$(pwd)/config":/etc/mysql/conf.d \
+       -v "$(pwd)/init_sql":/docker-entrypoint-initdb.d \
        -p "${MYSQL_PORT}":3306 \
        -p "${MYSQLX_PORT}":33060 \
        --network iuxt \
