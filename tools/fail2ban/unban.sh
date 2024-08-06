de7ed0ea43dacb21dcad7fcd05ad9e47f185943b
zhanglikun <x@zahui.fan>
2024-08-06 14:14:29 +0800

    优化脚本

--- tools/fail2ban/unban.sh
+++ tools/fail2ban/unban.sh
@@ -3,8 +3,9 @@ ip=120.136.158.158
 
 jail_list=$(fail2ban-client status | grep "Jail list" | awk -F ":" '{print $2}' | xargs | sed 's/,//g')
 
-for i in ${jail_list[*]};
+for i in "${jail_list[@]}";
+
 do
-    echo $i
-    fail2ban-client set ${i} unbanip ${ip}
+    echo "$i"
+    fail2ban-client set "${i}" unbanip "${ip}"
 done
