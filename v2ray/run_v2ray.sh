66cc3a8a7d40b8b445ad49f16a175e8c041bdec9
zhanglikun <iuxt@qq.com>
2024-08-19 14:14:11 +0800

    sync tz

--- v2ray/run_v2ray.sh
+++ v2ray/run_v2ray.sh
@@ -7,6 +7,7 @@ docker run -d --name v2ray \
   --network iuxt \
   --restart always \
   --mount type=bind,source="$(pwd)"/config.json,target=/v2ray/config.json \
+  --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
   iuxt/v2ray:v5.2.0
 
 ../public/add_config_to_nginx.sh
