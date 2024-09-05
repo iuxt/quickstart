97bbbe96d7a86b3700d1bd26568541f24b8cf006
zhanglikun <iuxt@qq.com>
2024-09-06 01:15:34 +0800

    fix fail2ban

--- frps/fail2ban/set_fail2ban.sh
+++ frps/fail2ban/set_fail2ban.sh
@@ -11,6 +11,7 @@ cp -f jail.d/frps.conf /etc/fail2ban/jail.d/
 container_id=$(docker inspect --format="{{.Id}}" frps)
 logpath=/var/lib/docker/containers/${container_id}/${container_id}-json.log
 sed -i "s#logpath = .*#logpath = ${logpath}#g" /etc/fail2ban/jail.d/frps.conf
+fail2ban-regex ${logpath} /etc/fail2ban/filter.d/frps.conf
 
 systemctl enable fail2ban
 systemctl reload fail2ban
