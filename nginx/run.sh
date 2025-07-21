ac4d795b60947b2b007b09705e997fe41074b4f4
zhanglikun <x@zahui.fan>
2025-07-21 23:53:01 +0800

    remova fail2ban

--- nginx/run.sh
+++ nginx/run.sh
@@ -10,18 +10,18 @@ docker rm -f nginx
 FORWARD_PORT=$(grep -h "listen" stream.d/*.conf | awk 'NF > 0 {print $NF}' | sed 's/;//')
 
 # 初始化一个变量存储端口映射
-DOCKER_PORT_MAPPING=""
+PORT_MAPPING=""
 
-# 遍历每个端口，拼接成 Docker 的端口映射格式
+# 遍历每个端口，拼接成 docker 的端口映射格式
 for i in $FORWARD_PORT; do
-    DOCKER_PORT_MAPPING="$DOCKER_PORT_MAPPING -p $i:$i"
+    PORT_MAPPING="$PORT_MAPPING -p $i:$i"
 done
 
 # 输出最终的结果
-echo $DOCKER_PORT_MAPPING
+echo $PORT_MAPPING
 
 
-docker run --name nginx \
+docker run -d --name nginx \
   -v ./www:/usr/share/nginx/html:ro \
   -v ./nginx.conf:/etc/nginx/nginx.conf \
   -v ./conf.d:/etc/nginx/conf.d \
@@ -31,13 +31,12 @@ docker run --name nginx \
   --mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
   -p 80:80 \
   -p 443:443 \
-  $DOCKER_PORT_MAPPING \
+  $PORT_MAPPING \
   --add-host=host.docker.internal:host-gateway \
   --network iuxt \
   --restart always \
+  --log-driver=json-file \
   --log-opt max-size=1G \
-  -d nginx:1.27.0
+  docker.io/nginx:1.27.0
 
-
-cd fail2ban && sudo ./set_fail2ban.sh
 
